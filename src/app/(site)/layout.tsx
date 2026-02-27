27f34bdd46cc41dff1f10658e560601cc156beee
Kiril15 <holydrip99@gmail.com>
2026-02-27 03:30:13 +0300

    fix date

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/(site)/Footer";
 import { Countdown } from "@/components/countdown/Countdown";
 
 export default function SiteLayout({ children }: { children: React.ReactNode }) {
-    const dropDate = new Date("2025-02-28T00:00:00");
+    const dropDate = new Date("2026-02-28T00:00:00");
     
     return (
         <Countdown date={dropDate}>
